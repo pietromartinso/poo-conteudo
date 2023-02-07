82bcf42523d29acf419d2e16760e5bef39ac049c
Pietro Martins de Oliveira <pietromartinso@gmail.com>
2023-02-07 09:45:43 -0300

    static member

--- poo-conteudo/ConteudoMinistrado.hpp
+++ poo-conteudo/ConteudoMinistrado.hpp
@@ -6,13 +6,13 @@
 
 class ConteudoMinistrado{
 	public:
+		static unsigned int proxId;
 		ConteudoMinistrado(std::string descricao, unsigned short cargaHorariaConteudo);
 
 		std::string& getDescricao();
 		unsigned short getCargaHorariaConteudo();
 		unsigned int getId();
 	private:
-		static unsigned int proxId;
 
 		std::string descricao;
 		unsigned short cargaHorariaConteudo;

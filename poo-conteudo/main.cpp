32ea80935b929465395a570bc380b60ff481da86
Pietro Martins de Oliveira <pietromartinso@gmail.com>
2023-02-07 10:38:06 -0300

    conteudo

--- poo-conteudo/main.cpp
+++ poo-conteudo/main.cpp
@@ -6,7 +6,7 @@
 #include "Console.hpp"
 
 int main(){
-	/*Pessoa prof1{"João", 40};
+	Pessoa prof1{"João", 40};
 	Disciplina dis1{"C++", nullptr};
 	dis1.setProfessor(&prof1);
 
@@ -15,15 +15,6 @@ int main(){
 
 	Console::imprimirDadosDisciplina(dis1);
 
-	return 0;*/
-
-	ConteudoMinistrado c1{ "Ponteiros", 4 };
-	ConteudoMinistrado c2{ "Referencias", 2 };
-
-	std::cout << c1.proxId << " " << c2.proxId << std::endl;
-	c1.proxId++;
-
-	std::cout << c1.proxId << " " << c2.proxId << std::endl;
 	return 0;
 
 }
